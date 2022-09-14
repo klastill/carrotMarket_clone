36cc43eb43fc0f8b7a949f627f37ab34348a4eb5
klastill <gyb0501@gmail.com>
2022-09-14 15:10:16 +0900

    add chat detail page

---
+++ pages/chats/[id].tsx
@@ -0,0 +1,144 @@
+import type { NextPage } from "next";
+
+const Chats: NextPage = () => {
+  return (
+    <div className="flex-1">
+      <div className="mb-12 space-y-4 px-4 py-10">
+        <div className="flex items-center space-x-2">
+          <div className="w-12 aspect-square bg-slate-200 rounded-full" />
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-gray-200">
+            Hi how much are you selling them for?
+          </div>
+        </div>
+        <div className="flex flex-row-reverse items-center space-x-2 space-x-reverse">
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-orange-400">
+            I want ￦20,000
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <div className="w-12 aspect-square bg-slate-200 rounded-full" />
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-gray-200">
+            미쳤어
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <div className="w-12 aspect-square bg-slate-200 rounded-full" />
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-gray-200">
+            Hi how much are you selling them for?
+          </div>
+        </div>
+        <div className="flex flex-row-reverse items-center space-x-2 space-x-reverse">
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-orange-400">
+            I want ￦20,000
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <div className="w-12 aspect-square bg-slate-200 rounded-full" />
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-gray-200">
+            미쳤어
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <div className="w-12 aspect-square bg-slate-200 rounded-full" />
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-gray-200">
+            Hi how much are you selling them for?
+          </div>
+        </div>
+        <div className="flex flex-row-reverse items-center space-x-2 space-x-reverse">
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-orange-400">
+            I want ￦20,000
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <div className="w-12 aspect-square bg-slate-200 rounded-full" />
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-gray-200">
+            미쳤어
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <div className="w-12 aspect-square bg-slate-200 rounded-full" />
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-gray-200">
+            Hi how much are you selling them for?
+          </div>
+        </div>
+        <div className="flex flex-row-reverse items-center space-x-2 space-x-reverse">
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-orange-400">
+            I want ￦20,000
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <div className="w-12 aspect-square bg-slate-200 rounded-full" />
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-gray-200">
+            미쳤어
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <div className="w-12 aspect-square bg-slate-200 rounded-full" />
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-gray-200">
+            Hi how much are you selling them for?
+          </div>
+        </div>
+        <div className="flex flex-row-reverse items-center space-x-2 space-x-reverse">
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-orange-400">
+            I want ￦20,000
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <div className="w-12 aspect-square bg-slate-200 rounded-full" />
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-gray-200">
+            미쳤어
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <div className="w-12 aspect-square bg-slate-200 rounded-full" />
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-gray-200">
+            Hi how much are you selling them for?
+          </div>
+        </div>
+        <div className="flex flex-row-reverse items-center space-x-2 space-x-reverse">
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-orange-400">
+            I want ￦20,000
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <div className="w-12 aspect-square bg-slate-200 rounded-full" />
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-gray-200">
+            미쳤어
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <div className="w-12 aspect-square bg-slate-200 rounded-full" />
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-gray-200">
+            Hi how much are you selling them for?
+          </div>
+        </div>
+        <div className="flex flex-row-reverse items-center space-x-2 space-x-reverse">
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-orange-400">
+            I want ￦20,000
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <div className="w-12 aspect-square bg-slate-200 rounded-full" />
+          <div className="w-1/2 text-sm fontmed text-gray-700 border rounded-xl px-4 py-2 border-gray-200">
+            미쳤어
+          </div>
+        </div>
+      </div>
+      <div className="fixed w-full px-4 pt-4 bottom-0 inset-x-0 bg-orange-50">
+        <div className="relative flex items-center mb-4">
+          <input
+            type="text"
+            className="appearance-none w-full px-4 py-2 pr-10 border border-gray-400 rounded-xl shadow-sm placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-orange-400 focus:border-transparent"
+          />
+          <div className="absolute inset-y-0 flex py-2 pr-2 right-0">
+            <button className="bg-orange-400 px-2 rounded-full text-white hover:bg-orange-500 cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:border-transparent">
+              &rarr;
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chats;
